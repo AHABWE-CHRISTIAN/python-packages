51716fa988c0d24ecfcb8ed7a71268845597b85b
AHABWE-CHRISTIAN <106030450+AHABWE-CHRISTIAN@users.noreply.github.com>
2022-06-11 23:03:50 +0300

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# using math module
+
+# ## GENERATING THE VALUE OF Pi.
+
+# In[1]:
+
+
+import math
+print(math.pi)
+
+
+# # using acos() methods
+
+# In[29]:
+
+
+from math import acos
+def printValueOfPi () :
+    Pi = round(2*acos(0.0), 3)
+    print (Pi)
+
+printValueOfPi () 
+
+
+# Using scipy
+
+# In[3]:
+
+
+import scipy
+scipy.pi
+print(scipy.pi)
+
+
+# QUESTION 2
+
+# In[2]:
+
+
+import math
+print (math.e)
+
+
+# In[3]:
+
+
+from math import e
+print (e)
+
+
+# #Using scipy
+
+# In[4]:
+
+
+import scipy
+scipy.e
+print (scipy.pi)
+
+
+# 
+
+# QUESTION 3
+
+# In[11]:
+
+
+import math
+math.cos(0)
+print (math.cos(0))
+
+
+# In[12]:
+
+
+from math import cos
+print (math.cos(0))
+
+
+# Using scipy
+
+# import scipy
+# scipy.cos(0)
+# print(scipy.cos(0))
+
+# QUESTION 4
+
+# In[16]:
+
+
+import math
+math.tan(45)
+print (math.tan(45))
+
+
+# In[15]:
+
+
+from math import tan
+print (math.tan(45))
+
+
+# In[ ]:
+
+
+Using scipy
+
+
+# In[8]:
+
+
+import scipy
+scipy.tan(45)
+print(scipy.tan(45))
+
+
+# In[ ]:
+
+
+
+
